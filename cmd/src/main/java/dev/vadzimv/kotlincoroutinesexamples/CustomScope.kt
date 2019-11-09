9a58f0d750d9eab771787e96a06d3d4eceb9afcd
vadzimv <vysotski.vadim@gmail.com>
2019-11-09 19:20:41 +0300

    custom scope

--- cmd/src/main/java/dev/vadzimv/kotlincoroutinesexamples/CustomScope.kt
+++ cmd/src/main/java/dev/vadzimv/kotlincoroutinesexamples/CustomScope.kt
@@ -6,25 +6,26 @@ import kotlin.coroutines.CoroutineContext
 suspend fun customScope() {
     val screen = Screen()
     screen.startTimer()
-    delay(100)
+    delay(15)
     screen.clear()
 }
 
 class Screen : CoroutineScope {
 
-    override val coroutineContext: CoroutineContext = Job() + Dispatchers.Default
+    override val coroutineContext: CoroutineContext =
+        Job() + Dispatchers.Default
+
+    fun clear() {
+        coroutineContext.cancel()
+    }
 
     fun startTimer() {
         var timerState = 0
         launch {
             while (true) {
-                delay(10)
+                delay(1)
                 println("timer ${timerState++}")
             }
         }
-    }
-
-    fun clear() {
-        coroutineContext.cancel()
     }
 }
